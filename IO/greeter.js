b5dc3d0d6d972c14bb4b9e6294624b38d1d3f915
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-02-21 11:42:24 -0500

    Add IO exercises

---
+++ IO/greeter.js
@@ -0,0 +1,8 @@
+let name = "Jane";
+console.log(`Good morning, ${name}!`);
+
+
+let rlSync = require('readline-sync');
+let firstName = rlSync.question('What is your first name? ');
+let lastName = rlSync.question('What is your last name? ');
+console.log(`Hello, ${firstName} ${lastName}!`);
