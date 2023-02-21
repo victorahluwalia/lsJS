34e791434e84838214505110ca9157ff2db8bf8d
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-02-21 10:53:25 -0500

    add variables exercises

---
+++ variables/age.js
@@ -0,0 +1,10 @@
+let name = 'Victor';
+console.log(`Good Morning, ${name}.`);
+console.log(`Good Afternoon, ${name}.`);
+console.log(`Good Evening, ${name}.`);
+
+let age = 33;
+console.log(`In 10 years, you will be ${age+10} years old.`);
+console.log(`In 20 years, you will be ${age+20} years old.`);
+console.log(`In 30 years, you will be ${age+30} years old.`);
+console.log(`In 40 years, you will be ${age+40} years old.`);
