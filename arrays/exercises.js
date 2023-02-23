b06cd9881d878aec54e58fdef5ecde24f4a00145
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-02-23 14:04:29 -0500

    Complete arrays exercises

--- arrays/exercises.js
+++ arrays/exercises.js
@@ -33,6 +33,7 @@ myArray.forEach(function(nestedArray) {
 })
 */
 
+/*
 let myArray = [
   1, 3, 6, 11,
   4, 2, 4, 9,
@@ -51,6 +52,7 @@ console.log(mapArray);
 
 let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
 let integers = [];
+
 function findIntegers(array) {
   return array.filter( function(element) {
     return Number.isInteger(element);
@@ -59,3 +61,69 @@ function findIntegers(array) {
 
 console.log(findIntegers(things)); // => [1, 3, -4]
  
+
+
+
+
+
+// Use map and filter to first determine the lengths of all the elements in an array of string values, 
+// then discard the even values (keep the odd values).
+
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+
+function oddLengths(array) {
+  let arrLengths = array.map( (phrase) => phrase.length)
+  let oddLengths = arrLengths.filter( (length) => length % 2 !== 0);
+  return oddLengths;
+}
+console.log(oddLengths(arr));
+
+
+////// 
+//////
+
+function sumOfSquares(arr) {
+  return arr.reduce((accumulator, currentValue) => { 
+    return accumulator + currentValue * currentValue;
+  }, 0);
+}
+
+let array = [3, 5, 7];
+console.log(sumOfSquares(array)); // => 83
+
+
+
+function oddLengths(array) {
+  return array.reduce(function(filteredArray, string) {
+    let length = string.length;
+
+    if (length % 2 === 1) {
+      filteredArray.push(length);
+    }
+
+    return filteredArray;
+  }, []);
+}
+
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr)); // => [1, 5, 3]
+
+
+
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];
+
+console.log(numbers1.includes(3));
+console.log(numbers2.includes(3));
+console.log(numbers3.includes(3));
+
+*/
+
+let arr = [
+  ["hello", "world"],
+  ["example", "mem", null, 6, 88],
+  [4, 8, 12]
+];
+
+arr[1][3] = 606;
