32d85aa1270004054c1faa74380c93f23df52ed9
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-02-28 18:12:22 -0500

    Complete JS100 exercises

--- js100/exercises.js
+++ js100/exercises.js
@@ -1,14 +1,22 @@
-function greeting() {
-  return 'Good morning';
+function clone(obj) {
+  return Object.assign({}, obj);
 }
 
-function recipient() {
-  return 'Launch School';
-}
-
-
-greet();
-function greet() {
-  console.log(`${greeting()}, ${recipient()}`)
+let person = {
+  title: 'Duke',
+  name: {
+    value: 'Nukem',
+    isStageName: true
+  },
+  age: 33
 };
 
+let clonedPerson = clone(person);
+person.age = 34;
+
+console.log(person.age);       // 34
+console.log(clonedPerson.age); // 33
+
+person.name.isStageName = false;
+console.log(person.name.isStageName);       // false
+console.log(clonedPerson.name.isStageName); // false
