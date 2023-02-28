3621cfd8584f702849a1f743f7f3cbf14c45629c
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-02-28 00:32:03 -0500

    More excercises

--- js100/exercises.js
+++ js100/exercises.js
@@ -1,28 +1,6 @@
-/*
-// Primitive data types
-console.log(Number.MAX_VALUE);
+let counter = 0;
 
-console.log(Date.now());
-
-let today = new Date();
-console.log(today.getYear());
-console.log(today.getFullYear());
-
-let speedLimit = 60;
-let currentSpeed = 80;
-
-if ((currentSpeed > speedLimit) && ((currentSpeed - speedLimit) > 5)) {
-  console.log('"People are so bad at driving cars ' +
-    'that computers don\'t have to be that good to be much better." ' +
-    '-- Marc Andreessen');
-}
-*/
-
-
-
-
-let tweet = 'Woohoo! :-)';
-
-if (tweet.length > 140) {
-  console.log('Tweet is too long!');
+while (counter > 0) {
+  console.log('Woooot!');
+  counter -= 1;
 }
