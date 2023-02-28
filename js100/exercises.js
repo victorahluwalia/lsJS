b5653e58748df2b61aec491f1b4430c92a64f322
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-02-28 16:54:44 -0500

    More exercises to JS100

--- js100/exercises.js
+++ js100/exercises.js
@@ -1,6 +1,22 @@
-let counter = 0;
+let vocabulary = [
+  ['happy', 'cheerful', 'merry', 'glad'],
+  ['tired', 'sleepy', 'fatigued', 'drained'],
+  ['excited', 'eager', 'enthused', 'animated']
+];
 
-while (counter > 0) {
-  console.log('Woooot!');
-  counter -= 1;
+// Expected output:
+// happy
+// cheerful
+// merry
+// etc...
+
+
+for (let i = 0; i < vocabulary.length; i++) {
+    let newArr = vocabulary[i];
+
+    for ( let x = 0; x < newArr.length; x++) {
+      console.log(newArr[x]);
+    }
+
+  
 }
