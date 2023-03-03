36cc745a7790463ffcb48b6fe9aca2321f015e38
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-03-03 14:04:33 -0500

    Add JSON configuration for messages

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -1,3 +1,4 @@
+const MESSAGES = require("./calculator_messages.json");
 const rlSync = require("readline-sync");
 
 function prompt(message) {
@@ -12,31 +13,31 @@ function validAnswerKeepGoing(answer) {
   return (answer === "y" || answer === "n");
 }
 
-prompt('Hi! Welcome to the calculator.');
+prompt(MESSAGES['welcome']);
 
 while (true) {
 
-prompt('Please enter the first number: ');
+prompt(MESSAGES['firstNumberPrompt']);
 let firstNumber = rlSync.question();
 
 while (invalidNumber(firstNumber)) {
-  prompt(`Hmm.. that doesn't look like a number. Please try entering again: `);
+  prompt(MESSAGES['invalidNumber']);
   firstNumber = rlSync.question();
 }
 
-prompt('Please enter the second number: ');
+prompt(MESSAGES['secondNumberPrompt']);
 let secondNumber = rlSync.question();
 
 while (invalidNumber(secondNumber)) {
-  prompt(`Hmm.. that doesn't look like a number. Please try entering again: `);
+  prompt(MESSAGES['invalidNumber']);
   secondNumber = rlSync.question();
 }
 
-prompt('What operation would you like to perform:\n 1: Add 2: Subtract 3: Multiply 4: Divide : ');
+prompt(MESSAGES['operation']);
 let operation = rlSync.question();
 
 while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('You must chose 1, 2, 3 or 4. Please try again: ');
+  prompt(MESSAGES['invalidOperation']);
   operation = rlSync.question();
 }
 
@@ -58,18 +59,18 @@ switch (operation) {
     break;
 }
 
-prompt(`The result of the calculation is: ${result}`);
+prompt(`${MESSAGES['result']} ${result}`);
 
-prompt('Would you like to perform another calculation ? (y/n)')
+prompt(MESSAGES['newCalculation']);
 let keepGoing = rlSync.question().toLowerCase();
 
 while (!validAnswerKeepGoing(keepGoing)) {
-  prompt('You seem to have entered an invalid choice. Please enter either of the following: y / n')
+  prompt(MESSAGES['invalidAnswer']);
   keepGoing = rlSync.question().toLowerCase();
 }
 
 if (keepGoing === "n") {
-  prompt('Thank you for using the calculator today.')
+  prompt(MESSAGES['End']);
   break;
 }
 
