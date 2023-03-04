d93a4456b2ca5331822029800ed7c73c917e409e
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-03-04 15:26:55 -0500

    Initialize Mortgage Calculator

---
+++ JS101/lesson_2/mortgage_calculator.js
@@ -0,0 +1,21 @@
+////// Inputs
+// Loan amount
+// Loan duration
+// APR
+
+//// Calculations
+// Loan duration in months
+// Monthly interest rate
+
+// Monthly payment (m)
+// let m = p * (j / (1 - Math.pow((1 + j), (-n))));
+// p (loan amount) ie. loanAmount
+// j (monthly interest rate)
+// n (loan duration in months)
+
+//// Output
+// Monthly payment (m) in float format with two decimals eg: $123.45
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const READLINESYNC = require("readline-sync");
