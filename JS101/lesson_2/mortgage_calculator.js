a237c34042111366a5cb039384ba5d174dcefed8
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-03-05 16:35:31 -0500

    Update mortgage calculator

--- JS101/lesson_2/mortgage_calculator.js
+++ JS101/lesson_2/mortgage_calculator.js
@@ -16,6 +16,87 @@
 //// Output
 // Monthly payment (m) in float format with two decimals eg: $123.45
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-const READLINESYNC = require("readline-sync");
+const readlineSync = require("readline-sync");
+const MESSAGES = {
+  welcome: 'Welcome to the mortgage calculator!',
+  askForLoanAmount: 'What is your required loan amount for the mortgage?',
+  invalidAmount: 'Please enter a valid loan amount. (Must be a number greater than zero)',
+  askForLoanDuration: 'How many years will be the mortgage?',
+  invalidDuration: 'Please enter a valid loan duration. (Must be a number greater than zero)',
+  askForApr: 'What is your APR? (For example: please input 6 for a 6% APR rate; for a 3.5% APR rate please input 3.5)',
+  invalidApr: 'Please enter a valid APR rate. (Must be a number greater than zero)',
+  giveMonthlyPayment: 'Your monthly payment will be : ',
+  newCalculation: 'Do you want to perform another calculation? (y/n)',
+  goodbye: 'Thank you for using the mortgage calculator.'
+};
+
+function messageKey(key) {
+  return MESSAGES[key];
+}
+
+function prompt(message) {
+  let text = messageKey(message);
+  console.log(`=> ${text}`);
+}
+
+function invalidLoanAmount(amount) {
+  return (amount.trimStart() === "" || Number.isNaN(Number(amount)) || Number(amount) <= 0);
+}
+
+function invalidLoanDuration(amount) {
+  return (amount.trimStart() === "" || Number.isNaN(Number(amount)) || Number(amount) <= 0);
+}
+
+function invalidAprRate(amount) {
+  return (amount.trimStart() === "" || Number.isNaN(Number(amount)) || Number(amount) <= 0);
+}
+
+prompt('welcome');
+
+while (true) {
+
+  prompt('askForLoanAmount');
+  let loanAmount = readlineSync.question();
+
+  while (invalidLoanAmount(loanAmount)) {
+    prompt('invalidAmount');
+    loanAmount = readlineSync.question();
+  }
+
+  prompt('askForLoanDuration');
+  let loanYears = readlineSync.question();
+  let loanMonths = loanYears * 12;
+
+  while (invalidLoanDuration(loanYears)) {
+    prompt('invalidDuration');
+    loanYears = readlineSync.question();
+  }
+
+  prompt('askForApr');
+  let annualRate = readlineSync.question();
+
+  while (invalidAprRate(annualRate)) {
+    prompt('invalidApr');
+    annualRate = readlineSync.question();
+  }
+  let monthlyRate = annualRate / (12 * 100);
+
+  let monthlyPayment = loanAmount *
+                      (monthlyRate /
+                      (1 - Math.pow((1 + monthlyRate), (-loanMonths)))
+                      );
+
+  prompt('giveMonthlyPayment');
+  console.log(`$ ${monthlyPayment.toFixed(2)}`);
+
+  prompt('newCalculation');
+  let keepCalculating = readlineSync.question();
+
+  if (keepCalculating === "n") {
+    prompt('goodbye');
+    break;
+  }
+
+}
