a91c0c74ff734b683ce36c047fcf99b7e83b8815
victorahluwalia <17994992+victorahluwalia@users.noreply.github.com>
2023-03-26 20:46:48 -0400

    Complete test

--- JS101/lesson_3/hard/StudyPlan.js
+++ JS101/lesson_3/hard/StudyPlan.js
@@ -1,10 +1,2 @@
 /*
-// Specific Topics of Interest
-In general, you should be familiar with JavaScript syntax and operators. 
-You should also be able to clearly explain, talk about, or demonstrate the following topics:
-
-
-
-
-discuss a function's use and purpose (a "user-level" description) instead of its implementation
-*/
+Interview prep
